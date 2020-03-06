c63d22bc2b41bccd1c2bb781b60f768a123f01d1
許益銘 <rc.701021@gmail.com>
2020-03-06 17:40:45 +0800

    update gulpfile; add a file for eample

--- gulpfile.js
+++ gulpfile.js
@@ -3,43 +3,56 @@ const cleanCss = require("gulp-clean-css");
 const eslint = require("gulp-eslint");
 const rename = require("gulp-rename");
 const sass = require('gulp-sass');
+const clean = require('gulp-clean');
 
 const { series, parallel, src, dest } = require('gulp');
 const file = 'jquery.nestable';
+const opt = {
+  suffix: '.min',
+  ext: {
+    js: '.js',
+    css: '.css',
+    scss: '.scss',
+    sass: '.sass'
+  },
+  path: {
+    dist: 'dist/'
+  }
+};
 
 function javascript (cb) {
-  src(file + '.js')
+  src(file + opt.ext.js)
     .pipe(uglify())
-    .pipe(rename({suffix: '.min'}))
-    .pipe(dest('dist/'));
+    .pipe(rename({suffix: opt.suffix}))
+    .pipe(dest(opt.path.dist));
   cb();
 }
 
 function sassTask (cb) {
-  src(file + '.scss')
+  src(file + opt.ext.scss)
     .pipe(sass().on('error', sass.logError))
     .pipe(dest('.'))
   cb();
 }
 
 function css (cb) {
-  src(file + '.css')
+  src(file + opt.ext.css)
     .pipe(cleanCss())
-    .pipe(rename({suffix: '.min'}))
-    .pipe(dest('dist/'));
+    .pipe(rename({suffix: opt.suffix}))
+    .pipe(dest(opt.path.dist));
   cb();
 };
 
 function test (cb) {
-  src([file + '.js'])
+  src([file + opt.ext.js])
     .pipe(eslint())
     .pipe(eslint.format())
     .pipe(eslint.failAfterError());
   cb();
 }
 
-function clean(cb) {
-  // body omitted
+function before_build (cb) {
+  //src(opt.path.dist, {read: false}).pipe(clean());
   cb();
 }
 
@@ -47,4 +60,4 @@ const build = parallel(series(sassTask, css), javascript)
   
 exports.test = test;
 exports.build = build;
-exports.default = series(clean, build);
+exports.default = series(before_build, build);
